2bec74f5739e91c5884709367b3d8522c74d7066
stacey <staceyamarks@gmail.com>
2017-12-27 17:51:24 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -20,6 +20,6 @@ function destructivelyAddElementToEndOfArray(array, element){
   return array;
 }
 
-accessElementInArray(array, index){
+function accessElementInArray(array, index){
   return array[index];
 }
