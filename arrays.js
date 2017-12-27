65a0181da956704b48cbda800dedd6ef5c769aa1
stacey <staceyamarks@gmail.com>
2017-12-27 17:56:07 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -29,4 +29,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
   return array;
 }
 
-
+function removeElementFromBeginningOfArray(array){
+  array = array.slice(1);
+  return array;
+}
