ca30aeb4dd3353cf4d475bc0a3a60c3cc5000d48
stacey <staceyamarks@gmail.com>
2017-12-27 17:49:05 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -9,3 +9,13 @@ function destructivelyAddElementToBeginningOfArray(array, element){
   array.unshift(element);
   return array;
 }
+
+function addElementToEndOfArray(array, element){
+  array = [...array, element];
+  return array;
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element);
+  return array;
+}
