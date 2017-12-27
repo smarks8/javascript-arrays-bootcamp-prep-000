38e99b34a84e8fa85463310538107b2820b4d528
stacey <staceyamarks@gmail.com>
2017-12-27 17:45:47 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,6 +6,6 @@ function addElementToBeginningOfArray(array, element){
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
-  array = array.unshift(element)
+  array[0] = element;
   return array;
 }
