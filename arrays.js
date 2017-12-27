593819e347b741b5e3818e2a7f1c930a0d0ab7b5
stacey <staceyamarks@gmail.com>
2017-12-27 18:00:04 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -39,4 +39,7 @@ function destructivelyRemoveElementFromEndOfArray(array){
   return array;
 }
 
-function 
+function removeElementFromEndOfArray(array){
+  array.slice(0,array.length-1);
+  return array;
+}
